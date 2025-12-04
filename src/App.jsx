4460bf6b11240ef7ff9187b03e2f9d276094fd68
morenonavarroluis <morenoluismanu@gmail.com>
2025-12-03 21:45:09 -0500

    agregar primer cambio

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export function App(){
+    return(
+       <article style={{ display:'flex' }}>
+        <header>
+            <img src="https://unavatar.io/midudev" alt="" />
+            <div>
+                <strong>Luis navarro</strong>
+                <span>@midudev@.vulnerado</span>
+            </div>
+        </header>
+       </article>
+    )
+}
